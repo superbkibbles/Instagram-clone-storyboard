16e71f38b8649f4e6b2b984e853cbdaf46689dd8
superbkibbles <mend_mzury@hotmail.com>
2024-11-14 10:55:57 +0300

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Mend Mzury on 11/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
